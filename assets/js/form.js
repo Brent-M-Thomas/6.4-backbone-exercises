ec5d2bb5e5f299d148248f484d6fdcce8e881a09
Brent Thomas <brentmt24@gmail.com>
2015-08-09 09:22:30 -0500

    still stuck.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,13 @@
 var FormDataModel = Backbone.Model.extend({
   idAttribute: '_id',
   defaults: {
-    title: ''
-  }
+    firstName: '',
+    lastName: '',
+    address: '',
+    phoneNumber: ''
+  },
+  urlRoot: 'http://tiny-lr.herokuapp.com/collections/bt-form'
+
 });
 
 var FormDataColl = Backbone.Collection.extend({
--- assets/js/views/app.js
+++ assets/js/views/app.js
@@ -13,12 +13,25 @@ var AppView = Backbone.View.extend({
   },
 
   render: function() {
-    var html = this.template(this.model.toJSON);
+    var html = this.template();
     this.$el.html(html);
     return this;
   },
 
   newForm: function() {
+    var firstName = this.$('.first-name').val();
+    var lastName = this.$('.last-name').val();
+    var address = this.$('.address').val();
+    var phoneNumber = this.$('.phone-number').val();
+
+    this.model.save({firstName: firstName, lastName: lastName, address: address, phoneNumber: phoneNumber});
+
+    this.$('.first-name').val('');
+    this.$('.last-name').val('');
+    this.$('.address').val('');
+    this.$('.phone-number').val('');
+
+    this.model = new FormDataModel();
 
   }
 
